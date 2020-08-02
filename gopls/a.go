9475b4c49846307389f5377f1df41b59a0e1c2ac
kurenaif <antyobido@gmail.com>
2020-08-03 00:14:34 +0900

    minimize a.go initialize

--- gopls/a.go
+++ gopls/a.go
@@ -21,7 +21,7 @@ func addContentLength(s string) string {
 }
 
 func initializeRequest(stdin io.WriteCloser) {
-	initialize := `{"id": 0, "jsonrpc": "2.0", "method": "initialize", "params":{"capabilities":{"textDocument":{"codeAction":{"codeActionLiteralSupport":{"codeActionKind":{"valueSet":["quickfix","refactor","refactor.extract","refactor.inline","refactor.rewrite","source","source.organizeImports"]}}},"codeLens":{"dynamicRegistration":true},"colorProvider":{"dynamicRegistration":false},"completion":{"completionItem":{"insertReplaceSupport":false,"snippetSupport":false}},"declaration":{"linkSupport":true},"definition":{"linkSupport":true},"hover":{},"implementation":{"linkSupport":true},"publishDiagnostics":{"relatedInformation":true},"semanticHighlightingCapabilities":{"semanticHighlighting":true},"signatureHelp":{"signatureInformation":{"parameterInformation":{"labelOffsetSupport":true}}},"typeDefinition":{"linkSupport":true}},"workspace":{"applyEdit":true,"didChangeWatchedFiles":{"dynamicRegistration":true}}},"clientInfo":{"name":"LanguageClient-neovim","version":"0.1.158 "},"processId":%d,"rootPath":"/home/kurenaif/Desktop/tools/gopls","rootUri":"file:///home/kurenaif/Desktop/tools/gopls","trace":"off"}}`
+	initialize := `{"id": 0, "jsonrpc": "2.0", "method": "initialize", "params":{"capabilities":null,"processId":%d,"rootPath":"/home/kurenaif/Desktop/tools/gopls","rootUri":"file:///home/kurenaif/Desktop/tools/gopls","trace":"off"}}`
 	initialize = fmt.Sprintf(initialize, os.Getpid())
 	initialize = addContentLength(initialize)
 	stdin.Write([]byte(initialize))
